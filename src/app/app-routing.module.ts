6b7c31301d7f74368a2fb26734b08085c06d3718
Nivetha <nive1802@gmail.com>
2024-02-28 19:52:04 +0530

    initial local1 commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { PasswordComponent } from './password/password.component';
+const routes: Routes = [
+  { path: 'Signup',component: SignUpFormComponent},
+  { path: 'Login' ,component: LoginComponent},
+  { path: 'Home' ,component: HomeComponent},
+  { path: 'Password',component: PasswordComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+ 
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { PasswordComponent } from './password/password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignUpFormComponent,
+    LoginComponent,
+    HomeComponent,
+    PasswordComponent
   ],
   imports: [
     BrowserModule,
